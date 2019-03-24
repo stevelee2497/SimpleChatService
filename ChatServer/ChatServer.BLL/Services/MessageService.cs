3bc73924171ebe7cd388f0488db7dd7563d3f951
Quoc Tran <Quoc.Tran@sioux.asia>
2019-03-24 22:32:06 +0700

    feat(Message): api create a message

--- ChatServer/ChatServer.BLL/Services/MessageService.cs
+++ ChatServer/ChatServer.BLL/Services/MessageService.cs
@@ -1,13 +1,42 @@
-﻿using ChatServer.BLL.Services.Base;
+﻿using System;
+using System.Net;
+using ChatServer.BLL.Requests;
+using ChatServer.BLL.Responses;
+using ChatServer.BLL.Services.Base;
+using ChatServer.DAL.Exceptions;
 using ChatServer.DAL.Models;
 
 namespace ChatServer.BLL.Services
 {
 	public interface IMessageService : IEntityService<Message>
 	{
+		BaseResponse Create(MessageRequest messageRequest);
 	}
 
 	public class MessageService : EntityService<Message>, IMessageService
 	{
+		public BaseResponse Create(MessageRequest messageRequest)
+		{
+			bool isSaved;
+			try
+			{
+				Create(new Message
+				{
+					UserConversationId = Guid.Parse(messageRequest.UserConversationId),
+					MessageContent = messageRequest.MessageContent
+				}, out isSaved);
+			}
+			catch (Exception e)
+			{
+				throw new BadRequestException(e.Message);
+			}
+
+			if (!isSaved)
+			{
+				throw new InternalServerErrorException("Ops, Something went wrong");
+			}
+
+			return new BaseResponse(HttpStatusCode.OK, data: "Created");
+		}
 	}
 }
---
+++ ChatServer/ChatServer/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using ChatServer.BLL.Requests;
+using ChatServer.BLL.Responses;
+using ChatServer.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatServer.Controllers
+{
+	[Route("api/messages")]
+	public class MessageController
+	{
+		private readonly IMessageService _messageService;
+
+		public MessageController(IMessageService messageService)
+		{
+			_messageService = messageService;
+		}
+
+		/// <summary>
+		/// Create a new message
+		/// </summary>
+		/// <param name="messageRequest"></param>
+		/// <returns>BaseResponse</returns>
+		[HttpPost]
+		[Produces("application/json")]
+		public BaseResponse Create([FromBody]MessageRequest messageRequest)
+		{
+			return _messageService.Create(messageRequest);
+		}
+	}
+}
